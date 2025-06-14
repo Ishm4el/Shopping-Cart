aaa5ed34d0cb2967d3f59c3e8defa95489846f28
Ismael Valenzuela <ismaelv@protonmail.com>
2025-06-13 20:05:25 -0700

    Implemented the NavigationMenu component for the main application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom";
 import NavigationMenu from "./components/NavigationMenu";
+import siteLogo from "/shopping_bag.svg";
+import styles from "./App.module.css";
+
+// CONFIG
+// title of the website
+const WEBSITE_TITLE = "Website Title";
+// links for the navigationbar
+const links: { title: string; hyperlink: string }[] = [
+  { title: "Home", hyperlink: "/home" },
+  { title: "Shop", hyperlink: "/shop" },
+];
 
 export default function App() {
   return (
     <>
-    <NavigationMenu />
-      <body>
+      <NavigationMenu title={WEBSITE_TITLE} logo={siteLogo} links={links} />
+      <body className={styles["body"]}>
         <Outlet />
       </body>
     </>
--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,3 +1,54 @@
-export default function NavigationMenu() {
-  return <></>;
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./NavigationMenu.module.css";
+
+export default function NavigationMenu({
+  logo,
+  title,
+  links,
+}: {
+  logo: string;
+  title: string;
+  links: { title: string; hyperlink: string }[];
+}) {
+  return (
+    <nav className={styles["nav"]}>
+      <Left logo={logo} title={title} />
+      <Right links={links} />
+    </nav>
+  );
+}
+
+function Left({ logo, title }: { logo: string; title: string }) {
+  const navigate = useNavigate();
+  return (
+    <div className={styles["left"]}>
+      <img
+        className={styles["img"]}
+        src={logo}
+        alt="Site_Logo"
+        onClick={() => navigate("/")}
+      />
+      <Link to={"/"} className={styles["link"]}>{title}</Link>
+    </div>
+  );
+}
+
+function RightLinks({ link }: { link: { title: string; hyperlink: string } }) {
+  return (
+    <li>
+      <Link to={link.hyperlink} className={styles["link"]}>{link.title}</Link>
+    </li>
+  );
+}
+
+function Right({ links }: { links: { title: string; hyperlink: string }[] }) {
+  return (
+    <div className={styles["right"]}>
+      <ul className={styles["ul"]}>
+        {links.map((link) => (
+          <RightLinks link={link} />
+        ))}
+      </ul>
+    </div>
+  );
 }
